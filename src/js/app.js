1382892b95e0d015bf99ebdee3abde8e5951c08e
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-02-29 23:05:48 +0300

    Added new custom element to learn how to drag it

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,3 @@
 import { MemoryGame } from './memory-game.js'
 import { QuizGameRef } from './component.js'
 import { Drag } from './drag.js'
-// const main = document.querySelector('.main')
-
-// const logoMem = document.querySelector('#logo-mem')
-// logoMem.addEventListener('click', event => {
-//   main.appendChild(new MemoryGame())
-// })
--- src/js/drag.js
+++ src/js/drag.js
@@ -2,39 +2,24 @@ const templateDrag = document.createElement('template')
 templateDrag.innerHTML = `
 <style>
 #container {
-  width: 100%;
-  height: 400px;
-  background-color: #333;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-  border-radius: 7px;
-  touch-action: none;
+  position: absolute;
+  z-index: 9;
+  background-color: #f1f1f1;
+  border: 1px solid #d3d3d3;
+  text-align: center;
 }
-#item {
-  width: 100px;
-  height: 100px;
-  background-color: rgb(245, 230, 99);
-  border: 10px solid rgba(136, 136, 136, .5);
-  border-radius: 50%;
-  touch-action: none;
-  user-select: none;
-}
-#item:active {
-  background-color: rgba(168, 218, 220, 1.00);
-}
-#item:hover {
-  cursor: pointer;
-  border-width: 20px;
-}
-</style>
-<div id="outerContainer">
-    <div id="container">
-      <div id="item">
 
-    </div>
-  </div>
+#header {
+  padding: 10px;
+  cursor: move;
+  z-index: 10;
+  background-color: #2196F3;
+  color: #fff;
+}
+
+</style>
+<div id="container">
+  <div id="header">Click</div>
 </div>
 `
 
@@ -45,73 +30,32 @@ export class Drag extends window.HTMLElement {
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(templateDrag.content.cloneNode(true))
 
-    this.dragItem = this.shadowRoot.querySelector('#item')
     this.container = this.shadowRoot.querySelector('#container')
+    this.header = this.shadowRoot.querySelector('#header')
 
-    this.active = false
-    this.currentX = 0
-    this.currentY = 0
-    this.initialX = 0
-    this.initialY = 0
-    this.xOffset = 0
-    this.yOffset = 0
+    this.pos1 = 0
+    this.pos2 = 0
+    this.pos3 = 0
+    this.pos4 = 0
   }
 
   connectedCallback () {
-    this.container.addEventListener('touchstart', this.dragStart, false)
-    this.container.addEventListener('touchend', this.dragEnd, false)
-    this.container.addEventListener('touchmove', this.drag, false)
-
-    this.container.addEventListener('mousedown', this.dragStart, false)
-    this.container.addEventListener('mouseup', this.dragEnd, false)
-    this.container.addEventListener('mousemove', this.drag, false)
+    this.header.addEventListener('click', event => {
+      this.dragMouseDown(event)
+      event.preventDefault()
+    })
   }
 
-  dragStart (e) {
-    console.log(e)
-    if (e.type === 'touchstart') {
-      this.initialX = e.touches[0].clientX - this.xOffset
-      this.initialY = e.touches[0].clientY - this.yOffset
-    } else {
-      this.initialX = e.clientX - this.xOffset
-      this.initialY = e.clientY - this.yOffset
-    }
-
-    if (e.target === this.dragItem) {
-      this.active = true
-    }
+  dragMouseDown (event) {
+    console.log(event)
+    event = event || window.event
+    this.pos3 = event.clientX
+    this.pos4 = event.clientY
   }
 
-  dragEnd (e) {
-    console.log(e)
-    this.initialX = this.currentX
-    this.initialY = this.currentY
-
-    this.active = false
-  }
-
-  drag (e) {
-    console.log(e)
-    if (this.active) {
-      e.preventDefault()
-
-      if (e.type === 'touchmove') {
-        this.currentX = e.touches[0].clientX - this.initialX
-        this.currentY = e.touches[0].clientY - this.initialY
-      } else {
-        this.currentX = e.clientX - this.initialX
-        this.currentY = e.clientY - this.initialY
-      }
-
-      this.xOffset = this.currentX
-      this.yOffset = this.currentY
-
-      this.setTranslate(this.currentX, this.currentY, this.dragItem)
-    }
-  }
-
-  setTranslate (xPos, yPos, el) {
-    el.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0)'
+  elementDrag (event) {
+    console.log(event)
+    event = event || window.event
   }
 }
 
