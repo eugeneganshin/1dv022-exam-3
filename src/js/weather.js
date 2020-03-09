8d6b45ef95d79682283cd0af273a3da5fa039fc2
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-09 13:52:24 +0300

    added icons to weather app

--- src/js/weather.js
+++ src/js/weather.js
@@ -5,6 +5,30 @@ export class WeatherApp extends window.HTMLElement {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(weatherTemplate.content.cloneNode(true))
+
+    this.container = this.shadowRoot.querySelector('.container')
+    this.header = this.shadowRoot.querySelector('.header')
+    this.notificationElement = this.shadowRoot.querySelector('.notification')
+    this.weatherContainer = this.shadowRoot.querySelector('.weather-container')
+    this.iconElement = this.shadowRoot.querySelector('.weather-icon')
+    this.tempElement = this.shadowRoot.querySelector('.temperature-value p')
+    this.descElement = this.shadowRoot.querySelector('.temperature-description p')
+    this.locationElement = this.shadowRoot.querySelector('.location p')
+
+    this.weather = {
+      temperature: {
+        value: 18,
+        unit: 'celsius'
+      },
+      description: 'few clouds',
+      iconId: '01d',
+      city: 'London',
+      country: 'GB'
+    }
+  }
+
+  connectedCallback () {
+
   }
 }
 
